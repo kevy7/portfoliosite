b6ea035b79b53d746ead1b81b277a2926c842a5c
kevy7 <kvnyang503@gmail.com>
2019-07-31 12:04:10 -0700

    created skills component

---
+++ src/skills/skills.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./skills.module.css";
+
+const Skills = () => {
+    return (
+        <div className={styles.skillsComponent}>
+
+        </div>
+    )
+}
+
+export default Skills;
