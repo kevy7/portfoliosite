45f74fd9c6f9293f315173ea56256c21abd59790
kevy7 <kvnyang503@gmail.com>
2019-07-30 22:34:15 -0700

    created and updated the projectContainer and project component.

--- src/projects/project.js
+++ src/projects/project.js
@@ -1,9 +1,9 @@
 import React from "react";
-import "./project.css";
+import styles from "./project.module.css";
 
-const Project = () => {
+const Project = (state) => {
     return (
-        <div className="projectComponent">
+        <div className={styles.projectComponent}>
             {/* 
                 Screenshot of project here 
                 Name of Project
@@ -11,6 +11,15 @@ const Project = () => {
                 Link to code
             */}
 
+            <img 
+                className={styles.projectImage}
+                src={state.image}
+            />
+            <h2 className={styles.projectTitle}>{state.projectName}</h2>
+            <ul className={styles.projectList}>
+                <li>Code</li>
+                <li>Website</li>
+            </ul>
         </div>
     )
 }
--- src/projects/projectsContainer.js
+++ src/projects/projectsContainer.js
@@ -1,11 +1,23 @@
 import React from "react";
 import Project from "./project";
+import styles from "./projectsContainer.module.css";
 
 //create a project component, this container will display all of your project components
 const ProjectsContainer = () => {
     return (
-        <div className="projectsContainer">
-            <p>This is the projects container</p>
+        <div className={styles.projectsContainer}>
+            <h1 className={styles.projectTitle}>My Projects</h1>
+            <div className={styles.projectContainerList}>
+                <Project
+                    image="https://images.unsplash.com/photo-1499673610122-01c7122c5dcb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+                    projectName="EventBubbles"
+                    projectLink=""
+                    projectCode=""
+                />
+                {/* <Project />
+                <Project />
+                <Project /> */}
+            </div>
         </div>
     )
 }
