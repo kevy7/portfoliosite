747e43704adc546067b26e99b669de5232e830a4
kevy7 <kvnyang503@gmail.com>
2019-07-30 22:41:56 -0700

    created the aboutMe component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,21 @@ import React from 'react';
 import Header from "./header/header";
 import NavBar from "./navBar/navBar";
 import ProjectsContainer from "./projects/projectsContainer";
+import AboutMe from "./aboutMe/aboutMe";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Header />
+      {/* Need to wrap the following components below to a container */}
       <ProjectsContainer />
+      <AboutMe />
+      {/* 
+        About Me 
+        Skills
+        Contact
+      */}
     </div>
   );
 }
---
+++ src/aboutMe/aboutMe.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./aboutMe.module.css";
+
+const AboutMe = () => {
+    return (
+        <div className={styles.aboutMe}>
+            <h1>About Me</h1>
+        </div>
+    )
+}
+
+export default AboutMe;
