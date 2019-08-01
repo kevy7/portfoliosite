59c8d2e4ec25ee364852b23558c6f279cd2f3427
kevy7 <kvnyang503@gmail.com>
2019-08-01 12:57:52 -0700

    updated the contact form and styled it

--- src/contactComponent/contact.js
+++ src/contactComponent/contact.js
@@ -5,8 +5,29 @@ const Contact = () => {
     return (
         <div className={styles.contactComponent}>
             <h1 className={styles.title}>Contact Me</h1>
-            <form action="https://formspree.io/email@domain.tld" method="POST">
-                <input type="text" />
+
+            <form className={styles.contactForm} action="https://formspree.io/email@domain.tld" method="POST">
+
+                <div className={styles.formComponent}>
+                    <label for="userEmail">Email</label>
+                    <input type="text" id="userEmail" />
+                </div>
+
+                <div className={styles.formComponent}>
+                    <label for="userName">Name:</label>
+                    <input type="text" id="userName" />
+                </div>
+
+                <div className={styles.formComponent}>
+                    <label for="userMessage">Message</label>
+                    <textarea rows="10" id="userMessage" />
+                </div>
+
+                <div className={styles.sendComponent}>
+                    <button className={styles.sendButton}>Send</button>
+                </div>
+
+
             </form>
 
 
