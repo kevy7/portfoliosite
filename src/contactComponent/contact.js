025e1c15a55b5a182a1fb210213dd46c56200dec
kevy7 <kvnyang503@gmail.com>
2019-08-01 20:44:54 -0700

    updated the form component. Now emails can be sent to me via the contact me form

--- src/contactComponent/contact.js
+++ src/contactComponent/contact.js
@@ -4,32 +4,29 @@ import styles from "./contact.module.css";
 const Contact = () => {
     return (
         <div className={styles.contactComponent}>
-            <h1 className={styles.title}>Contact Me</h1>
 
-            <form className={styles.contactForm} action="https://formspree.io/email@domain.tld" method="POST">
+            <form className={styles.contactForm} action="https://formspree.io/kvnyang503@gmail.com" method="POST">
+                <h1 className={styles.title}>Contact Me</h1>
 
                 <div className={styles.formComponent}>
                     <label for="userEmail">Email</label>
-                    <input className={styles.userInputs} type="text" id="userEmail" />
+                    <input className={styles.userInputs} type="email" name="email" id="userEmail" />
                 </div>
 
                 <div className={styles.formComponent}>
                     <label for="userName">Name</label>
-                    <input className={styles.userInputs} type="text" id="userName" />
+                    <input name="userName" className={styles.userInputs} type="text" id="userName" />
                 </div>
 
                 <div className={styles.formComponent}>
                     <label for="userMessage">Message</label>
-                    <textarea className={styles.userMessage} rows="10" id="userMessage" />
+                    <textarea name="message" placeholder="Write something in here..." className={styles.userMessage} rows="10" id="userMessage" />
                 </div>
 
                 <div className={styles.sendComponent}>
-                    <button className={styles.sendButton}>Send</button>
+                    <button type="submit" className={styles.sendButton}>Send</button>
                 </div>
-
-
             </form>
-
 
         </div>
     )
