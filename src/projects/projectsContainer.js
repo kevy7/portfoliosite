2889c022f7074ebe02ed33e93faf819f06280a51
kevy7 <kvnyang503@gmail.com>
2019-08-01 22:38:37 -0700

    added urls to code projects

--- src/projects/projectsContainer.js
+++ src/projects/projectsContainer.js
@@ -2,6 +2,8 @@ import React from "react";
 import Project from "./project";
 import styles from "./projectsContainer.module.css";
 import eBubblesImage from "../websiteImages/eBubbles.png";
+import rgbGameImage from "../websiteImages/rgbGame.png";
+import hnSearchCloneImage from "../websiteImages/hnSearchClone.png";
 
 
 //create a project component, this container will display all of your project components
@@ -12,21 +14,21 @@ const ProjectsContainer = () => {
             <div className={styles.projectContainerList}>
                 <Project
                     image={eBubblesImage}
-                    projectName="Full Stack Event Creation Website"
+                    projectName="EventBubbles"
                     projectLink="https://eb2.herokuapp.com/"
                     projectCode="https://github.com/kevy7/eBubblesv2"
                 />
                 <Project 
-                    image="https://images.unsplash.com/photo-1458682625221-3a45f8a844c7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+                    image={hnSearchCloneImage}
                     projectName="HN Search Clone"
                     projectLink=""
-                    projectCode=""
+                    projectCode="https://github.com/kevy7/hnClone"
                 />
                 <Project 
-                    image="https://images.unsplash.com/photo-1528360983277-13d401cdc186?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+                    image={rgbGameImage}
                     projectName="JS Color Game"
-                    projectLink=""
-                    projectCode=""
+                    projectLink="https://codepen.io/kevy7/full/wVqpmN"
+                    projectCode="https://codepen.io/kevy7/pen/wVqpmN"
                 />
                  {/* <Project />
                 <Project />
