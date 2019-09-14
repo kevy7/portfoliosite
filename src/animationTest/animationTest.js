77909ffcd783990d6aa8aef1f5be02b8b47a6332
kevy7 <kvnyang503@gmail.com>
2019-09-13 23:13:13 -0700

    still trying to figure out how to animate

--- src/animationTest/animationTest.js
+++ src/animationTest/animationTest.js
@@ -2,32 +2,46 @@ import React from 'react';
 import { Tween, Timeline} from 'react-gsap'; //used for animation
 import { Controller, Scene } from 'react-scrollmagic'; //used for scroll down effect
 
+import styles from './animationTest.module.css';
+import { Power0, Power2, Power3 } from 'gsap';
+import Header from '../header/header';
+
 const animationTest = () => {
     return (
-        <div className = "animationTest">
+        <div className = "{styles.animationTest}">
             <p className="para">This component is mainly used to test out animations</p>
             <Controller>
                 <Scene 
-                    triggerHook="onLeave"
-                    duration={3000}
-
-                    pin=".paraTest"
+                    TriggerHook="onLeave"
+                    //triggerElement=".tester"
+                    indicators={true}
+                    pin
+                    duration={1000}
                 >
-
-                    {(progress) => (
-                        <div className="tester">
-                            <Timeline totalProgress={progress} paused>
-                                <Tween
-                                    /* from={{ x: '10%', top: '60%' }} */
-                                    to={{ x: '60%', top: '10%' }}
-                                >
-                                    <p className="paraTest">This is a paragraph</p>
-
-                                </Tween>
+                    {
+                        (progress) => (
+                            <div>
+                                <Timeline 
+                                totalProgress={progress} paused
                                 
-                            </Timeline>
-                        </div>
-                    )}
+                                    target={
+                                        <div className={styles.headerTest}>This is the target</div>
+                                    }
+                                >
+                                    <Tween
+                                        from={{opacity: 0, top: '50px'}}
+                                        to={{opacity: 1, top: '0px' }}
+                                    />
+                                    <Tween 
+                                        to={{ x: '50px'}}
+                                    />
+                                    
+
+
+                                </Timeline>
+                            </div>
+                        )
+                    }
 
                 </Scene>
 
