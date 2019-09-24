4cee38e3ca3e4f02c29a5f77079a99b915022ccd
kevy7 <kvnyang503@gmail.com>
2019-09-24 00:01:45 -0700

    added my image to the about me section

--- src/animationTest/animationTest.js
+++ src/animationTest/animationTest.js
@@ -4,10 +4,12 @@ import { Controller, Scene } from 'react-scrollmagic'; //used for scroll down ef
 import { Power0, Power2, Power3 } from 'gsap';
 
 import styles from './animationTest.module.css';
+import chibiImage from '../images/ChibiKevin.jpg';
 
 const animationTest = () => {
     return (
         <div className = "{styles.mainDiv}">
+            {/* <img src={chibiImage} className={styles.chibiImage} /> */}
             <Controller>
                 <Scene 
                     triggerHook="onLeave"
@@ -15,7 +17,7 @@ const animationTest = () => {
                     //indicators={true}
                     pin="#test"
                     duration={1000}
-                >
+                >   
                     {
                         (progress) => (
                             <div className={styles.aboutMeContainer} id="test">
@@ -28,30 +30,38 @@ const animationTest = () => {
                                         <h1 className={styles.aboutMeHeader}>About Me</h1>
                                     </Tween>
 
-                                    <Tween
-                                        from={{ right:'200px',  opacity: 0}}
-                                        to={{ right: '0px', opacity: 1}}
-                                        duration="2000"
-                                    >
-                                        <p className={styles.aboutMeParagraph}>Hi, my name is Kevin Yang and I'm a web developer with experience in html, css, javascript, and the React library. </p>
-                                    </Tween>
+                                        <Tween
+                                            from={{ right:'200px',  opacity: 0}}
+                                            to={{ right: '0px', opacity: 1}}
+                                            duration="2000"
+                                        >
+                                            <div className={styles.container}>
+                                                <Tween
+                                                    to={{right: '350px'}}
+                                                >
 
-                                    <Tween
-                                        from={{ right:'200px',  opacity: 0}}
-                                        to={{ right: '0px', opacity: 1}}
-                                        duration="1000"
-                                    >
-                                        <p className={styles.aboutMeParagraph}>
-                                            My journey into programming started when I was given my first programming project at my previous job at Oregon State University. I was assigned the task to create a windows form application that can pull and enter instructor training-related data into a Microsoft SQL database.
-                                            After working on my project for hours at work, it made me realize how fun programming can be! Ever since then, even after graduating with a degree in MIS, I wanted to continue my education in programming by self-teaching myself current web developement technologies.
-                                            Every day after work, I would spend my free time reading about web developement technlogies, learning how to code on the front and backend, and working on my personal project.
-                                            Programming has since then become a hobby of mines and I would love to work for a company as a developer some day so that I can grow in the tech industry by doing what I love!
-                                        </p>
-                                    </Tween>
+                                                <img src={chibiImage} className={styles.chibiImage} />
+                                                </Tween>
+                                                <div>
+                                                <p className={styles.aboutMeParagraph}>Hi, my name is Kevin Yang and I'm a web developer with experience in html, css, javascript, and the React library. </p>
+                                                <p className={styles.aboutMeParagraph}> </p>
+                                                </div>
+                                            </div>
+                                        </Tween>
+
+                                    
 
                                 </Timeline>
                             </div>
                         )
+                    }
+
+                    {
+                        /* (progress) => (
+                            <div className={styles.container}>
+
+                            </div>
+                        ) */
                     }
 
                 </Scene>
