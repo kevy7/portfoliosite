536a7646ed962ee844b20b5ce596723b6423542c
kevy7 <kvnyang503@gmail.com>
2019-09-24 21:31:53 -0700

    updated styling to portfolio website

--- src/projects/projectDescription.js
+++ src/projects/projectDescription.js
@@ -6,7 +6,7 @@ const ProjectDescription = (state) => {
         <div className={styles.projectDescripton}>
             <h3>Description</h3>
             {state.projectDescription}
-            <h3>Technologies Used</h3>
+            <h3 className={styles.techHeader}>Technologies Used</h3>
             <ul className={styles.techList}>
                 {
                     state.projectTech.map((tech) => {
